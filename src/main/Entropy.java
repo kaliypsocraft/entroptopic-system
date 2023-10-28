3abb6128e3d1de6cd14b4ec6ace823b6ce59c1da
kalipysocraft <code_tradecraft@LAPTOP-J4V5OEI2>
2023-10-28 20:11:28 +1100

    Entropy function created

--- src/main/Entropy.java
+++ src/main/Entropy.java
@@ -1,5 +1,19 @@
 package main;
 
 public class Entropy {
-    
+    private double[] frequencies;
+    private static final int BINARY = 2;
+    public Entropy(double[] frequencies) {
+        this.frequencies = frequencies;
+    }
+    public double calculateEntropy() {
+        double entropy = 0.0;
+        for (double frequency : frequencies) {
+            entropy += - frequency * logBaseTwo(frequency);
+        }
+        return entropy;
+    }
+    public double logBaseTwo(double subject) {
+        return (Math.log(subject)/Math.log(BINARY));
+    }
 }
